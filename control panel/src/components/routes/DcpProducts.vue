0a957a1582f04b3264d0f225956b635151ddd00e
Faisul-Islam <suhid15@gmail.com>
2020-08-08 20:08:42 +0600

    misc

--- control panel/src/components/routes/DcpProducts.vue
+++ control panel/src/components/routes/DcpProducts.vue
@@ -3,17 +3,17 @@
     <v-container fluid>
       <v-row>
         <v-btn
-          color="green-sea-btn white--text"
-          class="mr-8 ml-8 mt-4 mb-4 rounded-lg"
+          color="green-sea-btn2 white--text"
+          class="mr-8 ml-8 mt-4 mb-4"
         >Add Categorie</v-btn>
       </v-row>
       <v-row>
-        <v-col v-for="(categorie, i) in categories" :key="i" lg="4" class>
+        <v-col v-for="(categorie, i) in categories" :key="i" cols="12" lg="4" md="6" sm="6" xs="12" class>
           <v-card min-height="410" color="white" min-width="260" class="rounded-t-lg no-gutters">
             <v-card-text class="green-sea-btn white--text text-center rounded-t-lg">{{categorie}}</v-card-text>
             <v-row class="pa-4">
-              <v-col v-for="(item,j) in items[i]" :key="j" lg="4" sm="6">
-                <v-card>
+              <v-col v-for="(item,j) in items[i]" :key="j" cols="6" lg="4" sm="4" xs="4">
+                <v-card elevation="1">
                   <v-hover v-slot:default="{ hover }">
                     <v-img :src="item.img" class="cursr" :aspect-ratio="16/9">
                       <v-expand-transition>
@@ -24,10 +24,11 @@
                       </v-expand-transition>
                     </v-img>
                   </v-hover>
-                  <v-card-text class="text-center pa-1 cursr">{{ item.name }}</v-card-text>
+                  <v-card-text class="text-center pa-1 cursr bold">{{ item.name }}</v-card-text>
                 </v-card>
               </v-col>
             </v-row>
+                              <v-card-actions ><v-btn text outlined color="green-sea-btn2" class="ma-auto text-center"> See More</v-btn></v-card-actions>
           </v-card>
         </v-col>
       </v-row>
@@ -37,7 +38,7 @@
 <script>
 export default {
   data: () => ({
-    categories: ["health", "food", "electronics"],
+    categories: ["health", "food", "electronics","text", "test"],
     items: [
       [
         {
