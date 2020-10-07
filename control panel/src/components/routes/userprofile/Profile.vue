140cd5e94280ce5b2b984097031037fae18e9f57
Injamamul Haque Sonet <ihsonnet@gmail.com>
2020-10-07 17:27:39 +0600

    all router added to profile page

--- control panel/src/components/routes/userprofile/Profile.vue
+++ control panel/src/components/routes/userprofile/Profile.vue
@@ -1,20 +1,104 @@
-<template>
-      <v-card class="mx-auto" max-width="1000" tile>
-          <v-img height="200" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"></v-img>
-          <v-row style="margin:2.5%;position:absolute; top: 130px">
 
-                      <v-list-item>
-                       <v-list-item-avatar size="100">
-                          <img
-                            src="https://www.w3schools.com/howto/img_avatar.png"
-                            alt="John"
-                          >
-                        </v-list-item-avatar>
-                        <v-list-item-content>
-                          <v-list-item-title class="title" style="margin-top:20px;">Injamamul Haque Sonet</v-list-item-title>
-                          <v-list-item-subtitle>Software Engineer | dokanee</v-list-item-subtitle>
-                        </v-list-item-content>
-                      </v-list-item>
-        </v-row>
-      </v-card>
-    </template>
+    <template>
+  <div class="block">
+    <el-timeline>
+      <el-timeline-item v-for="(item,index) of timeline" :key="index" :timestamp="item.timestamp" placement="top">
+        <el-card>
+          <h4>{{ item.title }}</h4>
+          <p>{{ item.content }}</p>
+        </el-card>
+      </el-timeline-item>
+    </el-timeline>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timeline: [
+        {
+          timestamp: '2019/4/20',
+          title: 'Update Github template',
+          content: 'PanJiaChen committed 2019/4/20 20:46'
+        },
+        {
+          timestamp: '2019/4/21',
+          title: 'Update Github template',
+          content: 'PanJiaChen committed 2019/4/21 20:46'
+        },
+        {
+          timestamp: '2019/4/22',
+          title: 'Build Template',
+          content: 'PanJiaChen committed 2019/4/22 20:46'
+        },
+        {
+          timestamp: '2019/4/23',
+          title: 'Release New Version',
+          content: 'PanJiaChen committed 2019/4/23 20:46'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.box-center {
+  margin: 0 auto;
+  display: table;
+}
+
+.text-muted {
+  color: #777;
+}
+
+.user-profile {
+  .user-name {
+    font-weight: bold;
+  }
+
+  .box-center {
+    padding-top: 10px;
+  }
+
+  .user-role {
+    padding-top: 10px;
+    font-weight: 400;
+    font-size: 14px;
+  }
+
+  .box-social {
+    padding-top: 30px;
+
+    .el-table {
+      border-top: 1px solid #dfe6ec;
+    }
+  }
+
+  .user-follow {
+    padding-top: 20px;
+  }
+}
+
+.user-bio {
+  margin-top: 20px;
+  color: #606266;
+
+  span {
+    padding-left: 4px;
+  }
+
+  .user-bio-section {
+    font-size: 14px;
+    padding: 15px 0;
+
+    .user-bio-section-header {
+      border-bottom: 1px solid #dfe6ec;
+      padding-bottom: 10px;
+      margin-bottom: 10px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
--- control panel/src/views/UserProfile.vue
+++ control panel/src/views/UserProfile.vue
@@ -5,10 +5,92 @@
         <v-main>
             <v-container class="pa-0 fill-height blue-grey lighten-5"  fluid>
                 <v-row class="fill-height">
-                    <v-col>
-                        <transition name="fade">
-                            <router-view></router-view>
-                        </transition>
+                    <v-col style="margin:3%">
+                        <template>
+                            <v-card class="mx-auto" max-width="1500" flat>
+                                <v-img height="200" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"></v-img>
+                                <v-row style="margin-left:2.5%;margin-top:-40px;position:related; top: 50px">
+
+                                            <v-list-item>
+                                            <v-list-item-avatar size="100">
+                                                <img
+                                                    src="https://www.w3schools.com/howto/img_avatar.png"
+                                                    alt="John"
+                                                >
+                                                </v-list-item-avatar>
+                                                <v-list-item-content>
+                                                <v-list-item-title class="title" style="margin-top:20px;">Abdullah Al Habib</v-list-item-title>
+                                                <v-list-item-subtitle>Software Engineer | Dokanee</v-list-item-subtitle>
+                                                </v-list-item-content>
+
+                                                <v-list-item-action>
+                                                <v-btn
+                                                    style="margin-top:40px; margin-right:40px;background-color: #2196F3; color: white"
+                                                    :class="fav ? 'white--text' : ''"
+                                                    icon-text
+                                                    @click="fav = !fav"
+                                                >
+                                                    <v-icon> mdi-plus-circle-outline</v-icon>
+                                                    <v-text>Follow</v-text>
+                                                </v-btn>
+                                                </v-list-item-action>
+                                            </v-list-item>
+                                </v-row>
+                            </v-card>
+                            <v-row>
+                                <v-col class="col-3">
+                                    <v-card flat>
+                                        <b>About</b>
+                                    </v-card>
+                                </v-col>
+                                <v-col>
+                                  
+                                    <v-card>
+                                        <v-tabs
+                                        v-model="tab"
+                                        background-color="matgreen accent-4"
+                                        centered
+                                        dark
+                                        icon-text
+                                        >
+                                        <v-tabs-slider></v-tabs-slider>
+
+                                        <v-tab to="./profile">
+                                            <v-icon size="25">mdi-account</v-icon>
+                                            <v-list-item-subtitle> Timeline</v-list-item-subtitle>
+                                        </v-tab>
+
+                                        <v-tab to="./edit-profile">
+                                            <v-icon>mdi-account-edit</v-icon>
+                                            <v-list-item-subtitle> Edit Profile</v-list-item-subtitle>
+                                        </v-tab>
+
+                                        <v-tab to="./settings">
+                                            <v-icon>mdi-account-settings</v-icon>
+                                            <v-list-item-subtitle> Account Settings</v-list-item-subtitle>
+                                        </v-tab>
+                                        </v-tabs>
+                                            <v-card flat>
+                                            <v-card-text>
+                                                <transition name="fade">
+                                                    <router-view></router-view>
+                                                </transition>
+                                            </v-card-text>
+                                            </v-card>
+                                    </v-card>
+                                    
+                                </v-col>
+                                <v-col class="col-3">
+                                        <v-card flat>
+                                            <b>My Stores</b>
+                                        </v-card>
+                                        <v-devider></v-devider>
+                                        <v-card flat>
+                                            <b>Followers</b>
+                                        </v-card>
+                                </v-col>
+                            </v-row>
+                        </template>
                     </v-col>
                 </v-row>
             </v-container>
