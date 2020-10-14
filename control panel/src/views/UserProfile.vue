88b5599a6091bb6a337d66a3f9dfba6eea1f6a38
Injamamul Haque Sonet <ihsonnet@gmail.com>
2020-10-14 19:17:55 +0600

    try profile api

--- control panel/src/views/UserProfile.vue
+++ control panel/src/views/UserProfile.vue
@@ -15,12 +15,12 @@
                                             <v-list-item-avatar size="100">
                                                 <img
                                                     src="https://www.w3schools.com/howto/img_avatar.png"
-                                                    alt="John"
+                                                    alt="Profile"
                                                 >
                                                 </v-list-item-avatar>
                                                 <v-list-item-content>
-                                                <v-list-item-title class="title" style="margin-top:20px;">Abdullah Al Habib</v-list-item-title>
-                                                <v-list-item-subtitle>Store Owner</v-list-item-subtitle>
+                                                <v-list-item-title class="title" style="margin-top:20px;">{{ userinfo.firstName }} {{ userinfo.lastName }}</v-list-item-title>
+                                                <v-list-item-subtitle>{{ userinfo.role}}</v-list-item-subtitle>
                                                 </v-list-item-content>
 
                                                 <v-list-item-action>
@@ -39,7 +39,38 @@
                             </v-card>
                             <v-row>
                                 <v-col class="col-3">
-                                    <about-card></about-card>
+                                    <v-card flat>
+                                    <div class="block">
+                                        <v-card-content>
+                                            <v-card-text style=" text-transform: uppercase;" >About</v-card-text>
+                                            <v-divider></v-divider>
+                                            <v-card-subtitle>
+                                                {{ userinfo.about }}
+                                            </v-card-subtitle>
+                                            <v-card-subtitle>
+                                                <b>Date of Birth:</b> {{ userinfo.dob }}
+                                                <b>Address:</b> <br> {{ userinfo.address }}
+                                                <br>
+                                                <b>Email:</b> <br> {{ userinfo.email }}
+                                                <br>
+                                                <b>Phone:</b> <br> {{ userinfo.phone }}
+                                            </v-card-subtitle>
+                                        </v-card-content>
+                                        <v-card-actions class="justify-center">
+                                        <v-btn
+                                            v-for="(social, i) in socials"
+                                            :key="i"
+                                            :color="social.color"
+                                            class="white--text"
+                                            fab
+                                            icon
+                                            small
+                                        >
+                                            <v-icon>{{ social.icon }}</v-icon>
+                                        </v-btn>
+                                        </v-card-actions>
+                                    </div>
+                                </v-card>
                                 </v-col>
                                 <v-col>
                                   
@@ -118,19 +149,62 @@
 </div>
 </template>
 <script>
+import axios from 'axios'
 import DcpNavDrawer from '@/components/DcpNavDrawer.vue'
-import AboutCard from '@/components/routes/userprofile/AboutCard.vue'
-
 export default {
+    data() {
+        return {
+        Auth: 'Bearer' + localStorage.getItem('access_token'),
+        userinfo: [],
+        // userinfo:  {
+        //     "role": "Store Owner",
+        //     "about": "This is about an user info. this is very importent. but here it a dummy text. we are working on it.",
+        //     "phone": "01734543027",
+        //     "address": "23/A Velanagar, Narsingdi, Dhaka, Bangladesh",
+        //     "dob": "31 May 1999",
+        //     "firstName": "Abdullah Al",
+        //     "lastName": "Habib",
+        //     "email": "aalhabib001@gmail.com"
+        // },
+        socials: [
+        {
+          icon: 'mdi-facebook',
+          color: 'indigo',
+        },
+        {
+          icon: 'mdi-linkedin',
+          color: 'cyan darken-1',
+        },
+        {
+          icon: 'mdi-instagram',
+          color: 'red lighten-3',
+        },
+      ]
+    }
+    },
+    methods: {
+      getImage () {
+        const min = 550
+        const max = 560
+
+        return Math.floor(Math.random() * (max - min + 1)) + min
+      },
+      userinfoAPI () {
+          var self = this
+          axios.get('https://dokanee-backend-monolithic.herokuapp.com/profile/getUserInfo', { 'headers': { 'Authorization': data().Auth }})
+          .then( response => {
+        console.log(response)
+    })
+      }
+    },
     components: {
-   'dcp-nav-drawer' : DcpNavDrawer,
-   'about-card' : AboutCard
+   'dcp-nav-drawer' : DcpNavDrawer
   },
   mounted(){
-      let token = localStorage.getItem('access_token');
+    this.userinfoAPI()
+    var token = localStorage.getItem('access_token');
     if(token == "") 
                window.location.href = "http://localhost:8080/#/auth/signin";
-
-  }
+  },
 }
 </script>
