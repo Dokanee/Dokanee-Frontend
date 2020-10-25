53872b892fd4e4ee4ff637ce3042efc7a8f147f4
Injamamul Haque Sonet <ihsonnet@gmail.com>
2020-10-25 15:23:52 +0600

    setup settings fields

--- control panel/src/components/routes/DcpSettings.vue
+++ control panel/src/components/routes/DcpSettings.vue
@@ -1,13 +1,97 @@
 <template>
-         <div class='blue-grey lighten-5 container-dashboard' >
+         <div class='blue-grey lighten-5 container-dashboard ma-1'>
              <v-container fluid>
                  <transition name="fade">
-                <v-row>
-                    <v-col style="position:absolute; top: 18% ; left: 30%;">
-                        <img centered style="width: 40%;" src="../../assets/comingsooon.png" alt="">
-                        <p style="text-align:center;width:40%;font-size:100%">Our team is working with this feature.
-                            Hope it will be available for you very soon.
-                            Stay with Dokanee.</p>
+                <v-row class="mb-12">
+                    <v-col :md="8">
+                        <v-row class="ma-1">
+                            <v-card flat width="1000px" class="pa-5">
+                            <div class="block">
+                            <v-card-title>
+                                    Edit Store Info
+                            </v-card-title>
+                            <v-card-content>
+
+                                <v-row>
+                                    <v-col>
+                                    <v-text-field type="text" :value="info.storeName" label="Store Name"></v-text-field>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col>
+                                    <v-text-field type="text" :value="info.storeDes" label="Description"></v-text-field>
+                                    </v-col>
+                                </v-row>
+                                <v-row>
+                                    <v-col>
+                                    <v-text-field type="text" :value="info.storeCat" label="Category"></v-text-field>
+                                    </v-col>
+                                </v-row>
+                                <label for="street">Store Address</label>
+                                    <v-row>
+                                        <v-col>
+                                            <v-text-field id="street" :value="info.address.street" label="Street"></v-text-field>
+                                        </v-col>
+                                        <v-col>
+                                            <v-text-field :value="info.address.dist" label="District"></v-text-field>
+                                        </v-col>
+                                        <v-col>
+                                            <v-text-field :value="info.address.div" label="Division"></v-text-field>
+                                        </v-col>
+                                    </v-row>
+                                <p>You have to enter your password to update info.</p>
+                                <v-text-field type="password" required label="Password"></v-text-field>
+                                <v-row>
+                                    <v-col class="pb-0 pt-5">
+                                        <v-btn class="float-right" color="matgreen white--text" @click="submit">Update</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-card-content>
+                            </div>
+                        </v-card>
+                        </v-row>
+                    </v-col>
+                    <v-col :md="4">
+                        <v-row class="ma-1">
+                            <v-card flat style="width:500px">
+                            <div class="block">
+                            <v-card-title>
+                                    Profile
+                            </v-card-title>
+                            <v-card-content>
+                                <div  class="pa-5" align="center">
+                                        <v-avatar size="80">
+                                        <img
+                                            src="https://www.w3schools.com/howto/img_avatar.png"
+                                            alt="John"
+                                        >
+                                        </v-avatar> <br> <br>
+
+                                        <v-col-content>
+                                            <b>Injamamul Haque Sonet</b> <br>
+                                            Store Owner <br>
+                                        </v-col-content> <br>
+                                        <button>
+                                            Edit Profile
+                                        </button>
+                                        <button>
+                                            Profile Settings
+                                        </button>
+                                </div>
+                            </v-card-content>
+                            </div>
+                        </v-card>
+                        </v-row>
+                        <v-row class="ma-1">
+                            <v-card flat style="margin-top:20px;width:500px">  
+                            <v-card-title>
+                                    Domain
+                            </v-card-title>
+                            <v-card-content>
+                               
+                            </v-card-content>
+                        </v-card>
+                        </v-row>
                     </v-col>
                 </v-row>
             </transition>
@@ -16,9 +100,40 @@
 </template>
 <script>
 export default {
+data () {
+        
+    return {
+        auth: 'Bearer ' + localStorage.getItem('access_token'),
+        selected: '',
+        info:  {
+            address: { 
+                street: "street 33/A",
+                dist: "Narsingdi",
+                div: "Dhaka"
+            },
+            
+            storeName: "AB Furniture",
+            storeDes: "Dummy store information.....What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            cat: null,
+            storeId: "",
+            storeCat: '',
+            categories: ['Dog', 'Cat', 'Rabbit', 'Turtle', 'Snake'],
+        },
+    }
+},
 
 }
 </script>
 <style scoped>
-
+button {
+    display: inline-block;
+    padding: 10px;
+    margin: 10px;
+    cursor: pointer;
+    color: white;
+    background-color: teal;
+    font-size: small;
+    justify-content: center;
+    margin: 5px;
+}
 </style>
